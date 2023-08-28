7c015750b18a145091d8ec003c20de30748aa4c5
Lpik97 <dm97fx@gmail.com>
2023-08-28 10:52:12 +0200

    feat(checkpasscoincidence function imported and invoked)
    The function will be invoked when the comfirm password input field receives any input.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import CHECK_ZIP from "./js_modules/checkZip.js";
 import CHECK_EMAIL from "./js_modules/checkEmail.js";
 import CHECK_PASSWORD from "./js_modules/checkPassword.js";
+import CHECK_PASS_COINCIDENCE from "./js_modules/checkPassCoincidence.js";
 
 window.onload = () => {
    document.getElementById("country").onchange = CHECK_ZIP;
    document.getElementById("zip-code").oninput = CHECK_ZIP;
    document.getElementById("email").oninput = CHECK_EMAIL;
    document.getElementById("password").oninput = CHECK_PASSWORD;
+   document.getElementById("comfirm-password").oninput = CHECK_PASS_COINCIDENCE;
 };
