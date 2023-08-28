95d36cd0c533320612d932561826c611b8add0ba
Lpik97 <dm97fx@gmail.com>
2023-08-28 09:53:32 +0200

    feat(contraints object)
    The contraints object has three properties, in each one there is a regExp that matches the desidered zip code, and a string that will appear when that case is not met.

---
+++ js_modules/constraints.js
@@ -0,0 +1,7 @@
+const CONSTRAINTS = {
+   arg: [/^[0-9]{4}$/, "Argentina ZIPs must have exactly 4 digits: e.g. 1950"],
+   br: [/^[0-9]{4}$/, "Brasil ZIPs must have exactly 4 digits: e.g. 2333"],
+   dk: [/^[0-9]{4}$/, "Denmark ZIPs must have exactly 4 digits: e.g. 2720"],
+};
+
+export default CONSTRAINTS;
