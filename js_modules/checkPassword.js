ed75769a39a53d37f029c33faf5085afc38e3cea
Lpik97 <dm97fx@gmail.com>
2023-08-28 10:36:22 +0200

    feat (check password function)
    This function will select both the password field and value, then create a regExp using the password property from constraints object and check if wether the password value matches the regExp inside the password property, throwing an error message if not.

---
+++ js_modules/checkPassword.js
@@ -0,0 +1,15 @@
+import CONSTRAINTS from "./constraints.js";
+
+const CHECK_PASSWORD = () => {
+   const PASSWORD_FIELD = document.getElementById("password");
+   const PASSWORD_VALUE = PASSWORD_FIELD.value;
+
+   const CONSTRAINT = new RegExp(CONSTRAINTS.password[0], "");
+   if (CONSTRAINT.test(PASSWORD_VALUE)) {
+      PASSWORD_FIELD.setCustomValidity("");
+   } else {
+      PASSWORD_FIELD.setCustomValidity(CONSTRAINTS.password[1]);
+   }
+};
+
+export default CHECK_PASSWORD;
