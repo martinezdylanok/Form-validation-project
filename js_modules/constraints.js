f8fb5ad6722d836cc2b34028546ebd4e1bf23c0e
Lpik97 <dm97fx@gmail.com>
2023-08-28 10:24:10 +0200

    fix(email regExp modified)

--- js_modules/constraints.js
+++ js_modules/constraints.js
@@ -2,7 +2,7 @@ const CONSTRAINTS = {
    arg: [/^[0-9]{4}$/, "Argentina ZIPs must have exactly 4 digits: e.g. 1950"],
    br: [/^[0-9]{4}$/, "Brasil ZIPs must have exactly 4 digits: e.g. 2333"],
    dk: [/^[0-9]{4}$/, "Denmark ZIPs must have exactly 4 digits: e.g. 2720"],
-   email: [/^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+com$/, "Please enter a valid email address using '@' and ending with '.com'"],
+   email: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$/, "Please enter a valid email address using '@' and ending with '.com'"],
 };
 
 export default CONSTRAINTS;
