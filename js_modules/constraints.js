e608e1e5bd152e80df49b70c6da86a839cd1eb3a
Lpik97 <dm97fx@gmail.com>
2023-08-28 10:34:33 +0200

    feat(email property added to the constraints object)

--- js_modules/constraints.js
+++ js_modules/constraints.js
@@ -3,6 +3,7 @@ const CONSTRAINTS = {
    br: [/^[0-9]{4}$/, "Brasil ZIPs must have exactly 4 digits: e.g. 2333"],
    dk: [/^[0-9]{4}$/, "Denmark ZIPs must have exactly 4 digits: e.g. 2720"],
    email: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$/, "Please enter a valid email address using '@' and ending with '.com'"],
+   password: [/^(?!.*\s).{8,16}$/, "Please enter a valid password with at minimum of 8 characters, and a max of 16"],
 };
 
 export default CONSTRAINTS;
