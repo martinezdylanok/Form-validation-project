fd06f3bc59c6312aefa0e6abedd7e81fc9ee64ee
Lpik97 <dm97fx@gmail.com>
2023-08-28 10:26:38 +0200

    feat(checkEmail function imported and invoked)
    The function will be invoked when the input field of email changes.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import CHECK_ZIP from "./js_modules/checkZip.js";
+import CHECK_EMAIL from "./js_modules/checkEmail.js";
 
 window.onload = () => {
    document.getElementById("country").onchange = CHECK_ZIP;
    document.getElementById("zip-code").oninput = CHECK_ZIP;
+   document.getElementById("email").oninput = CHECK_EMAIL;
 };
