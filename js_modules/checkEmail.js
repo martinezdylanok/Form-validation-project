163034da0d9c98eafec800fb27cad924930db8b5
Lpik97 <dm97fx@gmail.com>
2023-09-01 08:59:15 +0200

    feat(new if statement checking for empty string)
    A new if statement was added checking for an empty string in the email input field value.

--- js_modules/checkEmail.js
+++ js_modules/checkEmail.js
@@ -4,11 +4,15 @@ const CHECK_EMAIL = () => {
    const EMAIL_FIELD = document.getElementById("email");
    const EMAIL_VALUE = EMAIL_FIELD.value;
 
-   const CONSTRAINT = new RegExp(CONSTRAINTS.email[0]);
-   if (CONSTRAINT.test(EMAIL_VALUE)) {
-      EMAIL_FIELD.setCustomValidity("");
+   if (EMAIL_VALUE.trim() === "") {
+      EMAIL_FIELD.setCustomValidity("Email cannot be empty");
    } else {
-      EMAIL_FIELD.setCustomValidity(CONSTRAINTS.email[1]);
+      const CONSTRAINT = new RegExp(CONSTRAINTS.email[0]);
+      if (CONSTRAINT.test(EMAIL_VALUE)) {
+         EMAIL_FIELD.setCustomValidity("");
+      } else {
+         EMAIL_FIELD.setCustomValidity(CONSTRAINTS.email[1]);
+      }
    }
 };
 
