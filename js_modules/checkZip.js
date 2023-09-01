138d752e90f34c38de69b6822943a7f8cde2411c
Lpik97 <dm97fx@gmail.com>
2023-09-01 09:05:29 +0200

    feat(new if statement checking for empty string)
    A new if statement was added checking for an empty string in the zipcode input field value.

--- js_modules/checkZip.js
+++ js_modules/checkZip.js
@@ -4,11 +4,15 @@ const CHECK_ZIP = () => {
    const COUNTRY = document.getElementById("country").value;
    const ZIP_FIELD = document.getElementById("zip-code");
 
-   const CONSTRAINT = new RegExp(CONSTRAINTS[COUNTRY][0]);
-   if (CONSTRAINT.test(ZIP_FIELD.value)) {
-      ZIP_FIELD.setCustomValidity("");
+   if (ZIP_FIELD.value.trim() === "") {
+      ZIP_FIELD.setCustomValidity("ZIP code cannot be empty");
    } else {
-      ZIP_FIELD.setCustomValidity(CONSTRAINTS[COUNTRY][1]);
+      const CONSTRAINT = new RegExp(CONSTRAINTS[COUNTRY][0]);
+      if (CONSTRAINT.test(ZIP_FIELD.value)) {
+         ZIP_FIELD.setCustomValidity("");
+      } else {
+         ZIP_FIELD.setCustomValidity(CONSTRAINTS[COUNTRY][1]);
+      }
    }
 };
 
