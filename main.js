df0b6e31c9153cea1421be78afb12e39b55ac53c
Lpik97 <dm97fx@gmail.com>
2023-09-01 10:12:47 +0200

    feat(function refactory)
    All the input fields will be selected on load as well as the form, to then add an event listener waiting for a blur or input events and calling the check depending on which input it is. For the form element, the validate form function will be called.

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ import CHECK_ZIP from "./js_modules/checkZip.js";
 import CHECK_EMAIL from "./js_modules/checkEmail.js";
 import CHECK_PASSWORD from "./js_modules/checkPassword.js";
 import CHECK_PASS_COINCIDENCE from "./js_modules/checkPassCoincidence.js";
+import VALIDATE_FORM from "./js_modules/validateForm.js";
 
 window.onload = () => {
-   document.getElementById("country").onchange = CHECK_ZIP;
-   document.getElementById("zip-code").oninput = CHECK_ZIP;
-   document.getElementById("email").oninput = CHECK_EMAIL;
-   document.getElementById("password").oninput = CHECK_PASSWORD;
-   document.getElementById("comfirm-password").oninput = CHECK_PASS_COINCIDENCE;
+   const ZIP_FIELD = document.getElementById("zip-code");
+   const EMAIL_FIELD = document.getElementById("email");
+   const PASSWORD_FIELD = document.getElementById("password");
+   const CONFIRM_PASSWORD_FIELD = document.getElementById("confirm-password");
+   const FORM = document.getElementById("form");
+
+   ZIP_FIELD.addEventListener("blur", CHECK_ZIP);
+   ZIP_FIELD.addEventListener("input", CHECK_ZIP);
+
+   EMAIL_FIELD.addEventListener("blur", CHECK_EMAIL);
+   EMAIL_FIELD.addEventListener("input", CHECK_EMAIL);
+
+   PASSWORD_FIELD.addEventListener("blur", CHECK_PASSWORD);
+   PASSWORD_FIELD.addEventListener("input", CHECK_PASSWORD);
+
+   CONFIRM_PASSWORD_FIELD.addEventListener("blur", CHECK_PASS_COINCIDENCE);
+   CONFIRM_PASSWORD_FIELD.addEventListener("input", CHECK_PASS_COINCIDENCE);
+
+   FORM.addEventListener("submit", VALIDATE_FORM);
 };
